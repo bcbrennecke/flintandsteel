2498c4146e0f6f60e1b23578dda45fcefec60458
Braun Brennecke <bcbrennecke@gmail.com>
2016-10-08 22:43:23 -0400

    Added 4 charts for stages of an idea with sample data

--- src/metrics/metrics.js
+++ src/metrics/metrics.js
@@ -11,27 +11,82 @@ angular.module('flintAndSteel')
 
 
 
-            google.charts.load('current', {'packages':['corechart']});
+        google.charts.load('current', {'packages':['corechart']});
         google.charts.setOnLoadCallback(drawChart);
 
         function drawChart() {
-          var data = google.visualization.arrayToDataTable([
-            ['Year', 'Sales', 'Expenses'],
-            ['2004',  1000,      400],
-            ['2005',  1170,      460],
-            ['2006',  660,       1120],
-            ['2007',  1030,      540]
+
+
+          var newIdeaData = google.visualization.arrayToDataTable([
+            ['Month', 'Ideas Created'],
+            ['Jan',  100],
+            ['Feb',  87],
+            ['Mar',  105],
+            ['Apr',  94]
           ]);
 
-          var options = {
-            title: 'Company Performance',
+          var prototypedIdeaData = google.visualization.arrayToDataTable([
+            ['Month', 'Ideas Prototyped'],
+            ['Jan',  10],
+            ['Feb',  15],
+            ['Mar',  12],
+            ['Apr',  8]
+          ]);
+
+          var fundedIdeaData = google.visualization.arrayToDataTable([
+            ['Month', 'Ideas Funded'],
+            ['Jan',  4],
+            ['Feb',  3],
+            ['Mar',  5],
+            ['Apr',  2]
+          ]);
+
+          var completedIdeaData = google.visualization.arrayToDataTable([
+            ['Month', 'Ideas Completed'],
+            ['Jan',  2],
+            ['Feb',  3],
+            ['Mar',  1],
+            ['Apr',  4]
+          ]);
+
+
+
+          var newIdeaOptions = {
+            title: 'New Ideas Generated',
             curveType: 'function',
             legend: { position: 'bottom' }
           };
 
-          var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+          var prototypedIdeaOptions = {
+            title: 'Ideas Prototyped',
+            curveType: 'function',
+            legend: { position: 'bottom' }
+          };
 
-          chart.draw(data, options);
+          var fundedIdeaOptions = {
+            title: 'Ideas Funded',
+            curveType: 'function',
+            legend: { position: 'bottom' }
+          };
+
+          var completedIdeaOptions = {
+            title: 'Ideas Completed',
+            curveType: 'function',
+            legend: { position: 'bottom' }
+          };
+
+
+
+          var newIdeas = new google.visualization.LineChart(document.getElementById('new_ideas_chart'));
+          var prototypedIdeas = new google.visualization.LineChart(document.getElementById('prototyped_ideas_chart'));
+          var fundedIdeas = new google.visualization.LineChart(document.getElementById('funded_ideas_chart'));
+          var completedIdeas = new google.visualization.LineChart(document.getElementById('completed_ideas_chart'));
+
+
+          newIdeas.draw(newIdeaData, newIdeaOptions);
+          prototypedIdeas.draw(prototypedIdeaData, prototypedIdeaOptions);
+          fundedIdeas.draw(fundedIdeaData, fundedIdeaOptions);
+          completedIdeas.draw(completedIdeaData, completedIdeaOptions);
         }
 
         }
